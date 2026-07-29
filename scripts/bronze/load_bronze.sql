94f269a679cf54f57aa7818e62fa0f6aeb50b961
RIYAS KHAN <riyastech001@gmail.com>
2026-07-29 17:12:00 +0530

    Revise data loading instructions in load_bronze.sql
    Updated instructions for loading data in pgAdmin.

--- scripts/bronze/load_bronze.sql
+++ scripts/bronze/load_bronze.sql
@@ -11,9 +11,8 @@ Script Purpose:
     Due to local laptop server permission restrictions, automated scripts cannot
     access local directory paths. Data must be imported using the GUI wizard.
 
-How to Load Data in pgAdmin:
-    1. Run this entire script to create the 6 empty tables.
-    2. Right-click a table in pgAdmin (e.g., crm_cust_info).
+ Load Data in pgAdmin:
+    1.  create the 6 empty tables.
     3. Select 'Import/Export data...'.
     4. Set the toggle to 'Import', select your CSV file, and choose 'CSV' format.
     5. Turn on the 'Header' option and click 'OK'.
