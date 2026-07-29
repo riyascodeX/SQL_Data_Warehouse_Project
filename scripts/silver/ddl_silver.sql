7c4d96fd1cf96dda424ed3852bb64f12986c1347
RIYAS KHAN <riyastech001@gmail.com>
2026-07-29 16:47:14 +0530

    Create ETL script for silver schema tables

--- scripts/silver/ddl_silver.sql
+++ scripts/silver/ddl_silver.sql
@@ -1,3 +1,22 @@
+/*===============================================================================
+STORED PROCEDURE: load_silver (Bronze -> Silver)
+===============================================================================
+Script Purpose:
+    This script performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables.
+	
+Actions Performed:
+	- Drops existing bronze tables if they exist.
+	- Re-creates silver tables with proper schemas and metadata.
+		
+Parameters:
+    None.
+===============================================================================*/
+
+
+-- ============================================================================
+-- 1. TABLE: silver.crm_cust_info
+-- ============================================================================
 DROP TABLE IF EXISTS silver.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
     cst_id              INT,
@@ -10,7 +29,9 @@ CREATE TABLE bronze.crm_cust_info (
     dwh_create_date     TIMESTAMPTZ DEFAULT now()
 );
 
-
+-- ============================================================================
+-- 2. TABLE: silver.crm_prd_info
+-- ============================================================================
 DROP TABLE IF EXISTS silver.crm_prd_info;
 CREATE TABLE silver.crm_prd_info (
     prd_id       INT,
@@ -24,7 +45,9 @@ CREATE TABLE silver.crm_prd_info (
     dwh_create_date     TIMESTAMPTZ DEFAULT now()
 );
 
-
+-- ============================================================================
+-- 3. TABLE: silver.crm_sales_details
+-- ============================================================================
 DROP TABLE IF EXISTS silver.crm_sales_details;
 CREATE TABLE silver.crm_sales_details (
     sls_ord_num  VARCHAR(50),
@@ -39,7 +62,9 @@ CREATE TABLE silver.crm_sales_details (
     dwh_create_date     TIMESTAMPTZ DEFAULT now()
 );
 
-
+-- ============================================================================
+-- 4. TABLE: silver.erp_loc_a101
+-- ============================================================================
 DROP TABLE IF EXISTS silver.erp_loc_a101;
 CREATE TABLE silver.erp_loc_a101 (
     cid    VARCHAR(50),
@@ -47,7 +72,9 @@ CREATE TABLE silver.erp_loc_a101 (
     dwh_create_date     TIMESTAMPTZ DEFAULT now()
 );
 
-
+-- ============================================================================
+-- 5. TABLE: silver.erp_cust_az12
+-- ============================================================================
 DROP TABLE IF EXISTS silver.erp_cust_az12;
 CREATE TABLE silver.erp_cust_az12 (
     cid    VARCHAR(50),
@@ -56,7 +83,9 @@ CREATE TABLE silver.erp_cust_az12 (
     dwh_create_date     TIMESTAMPTZ DEFAULT now()
 );
 
-
+-- ============================================================================
+-- 6. TABLE: silver.erp_px_cat_g1v2
+-- ============================================================================
 DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
 CREATE TABLE silver.erp_px_cat_g1v2 (
     id           VARCHAR(50),
