b3c8968a5e1f8133d8791f8b61c0ae24ce1d8ef6
RIYAS KHAN <riyastech001@gmail.com>
2026-07-29 16:54:28 +0530

    Create DDL script for bronze schema tables
    Removed existing tables before creating new ones for customer, product, sales, and ERP location information.

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -12,9 +12,8 @@ Parameters:
     None.
 ============================================================================== */
 
--- ============================================================================
+
 -- 1. TABLE: bronze.crm_cust_info
--- ============================================================================
 DROP TABLE IF EXISTS bronze.crm_cust_info;
 CREATE TABLE bronze.crm_cust_info (
     cst_id              INT,
@@ -26,9 +25,8 @@ CREATE TABLE bronze.crm_cust_info (
     cst_create_date     DATE
 );
 
--- ============================================================================
+
 -- 2. TABLE: bronze.crm_prd_info
--- ============================================================================
 DROP TABLE IF EXISTS bronze.crm_prd_info;
 CREATE TABLE bronze.crm_prd_info (
     prd_id       INT,
@@ -40,9 +38,8 @@ CREATE TABLE bronze.crm_prd_info (
     prd_end_date   DATE
 );
 
--- ============================================================================
+
 -- 3. TABLE: bronze.crm_sales_details
--- ============================================================================
 DROP TABLE IF EXISTS bronze.crm_sales_details;
 CREATE TABLE bronze.crm_sales_details (
     sls_ord_num  VARCHAR(50),
@@ -56,18 +53,16 @@ CREATE TABLE bronze.crm_sales_details (
     sls_price    INT
 );
 
--- ============================================================================
+
 -- 4. TABLE: bronze.erp_loc_a101
--- ============================================================================
 DROP TABLE IF EXISTS bronze.erp_loc_a101;
 CREATE TABLE bronze.erp_loc_a101 (
     cid    VARCHAR(50),
     country  VARCHAR(50)
 );
 
--- ============================================================================
+
 -- 5. TABLE: bronze.erp_cust_az12
--- ============================================================================
 DROP TABLE IF EXISTS bronze.erp_cust_az12;
 CREATE TABLE bronze.erp_cust_az12 (
     cid    VARCHAR(50),
@@ -75,9 +70,8 @@ CREATE TABLE bronze.erp_cust_az12 (
     gen    VARCHAR(50)
 );
 
--- ============================================================================
+
 -- 6. TABLE: bronze.erp_px_cat_g1v2
--- ============================================================================
 DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
 CREATE TABLE bronze.erp_px_cat_g1v2 (
     id           VARCHAR(50),
